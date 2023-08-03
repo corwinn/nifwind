ac6b5d0838a7aa6f658feae8c3156f6b49730030
corwinn <corwinn@users.noreply.github.com>
2023-08-03 12:15:05 +0300

    cc: apply another rule

---
+++ src/services/n_options.h
@@ -0,0 +1,9 @@
+// You can put things here. And you can get them. You could even get notified
+// when things you put here got changed - neat.
+// Things you put here could get persisted.
+//
+// This is a key-value store: means you have to be careful with the keys: a
+// duplicate one will reward you with an exception. Recommended usage:
+//   object_name.domain.sub-domain.data_name
+//   example: "mainform.dockwidgets.treeview.x"
+// A non existent one will reward you with an exception as well.
